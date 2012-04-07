84ce58ab51a527bda287450ddb2d0a738a0db7ba
ajlopez <webmaster@ajlopez.com>
2012-04-07 17:09:27 -0300

    DartSharp console

---
+++ Src/DartSharp.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DartSharp.Compiler;
+using DartSharp.Commands;
+using DartSharp.Methods;
+
+namespace DartSharp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("DartSharp 0.0.0");
+            Lexer lexer = new Lexer(System.Console.In);
+            Context context = new Context();
+            context.SetValue("print", new Print(System.Console.Out));
+            Parser parser = new Parser(lexer);
+
+            for (ICommand cmd = parser.ParseCommand(); cmd != null; cmd = parser.ParseCommand())
+                cmd.Execute(context);
+        }
+    }
+}

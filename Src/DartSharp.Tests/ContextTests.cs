e57b35befae559594388c4190883d14561424fe9
ajlopez <webmaster@ajlopez.com>
2012-04-07 11:34:46 -0300

    Context with first tests

---
+++ Src/DartSharp.Tests/ContextTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DartSharp.Tests
+{
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUndefinedValue()
+        {
+            Context context = new Context();
+            Assert.IsNull(context.GetValue("Foo"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            Context context = new Context();
+            context.SetValue("One", 1);
+            Assert.AreEqual(1, context.GetValue("One"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValueWithParent()
+        {
+            Context parent = new Context();
+            Context context = new Context(parent);
+            context.SetValue("One", 1);
+            Assert.AreEqual(1, context.GetValue("One"));
+            Assert.IsNull(parent.GetValue("One"));
+        }
+
+        [TestMethod]
+        public void GetValueFromParent()
+        {
+            Context parent = new Context();
+            Context context = new Context(parent);
+            parent.SetValue("One", 1);
+            Assert.AreEqual(1, context.GetValue("One"));
+        }
+    }
+}
+
---
+++ Src/DartSharp/Context.cs
@@ -0,0 +1,38 @@
+﻿namespace DartSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Context
+    {
+        private Context parent;
+        private Dictionary<string, object> values = new Dictionary<string, object>();
+
+        public Context()
+        {
+        }
+
+        public Context(Context parent)
+        {
+            this.parent = parent;
+        }
+
+        public void SetValue(string name, object value)
+        {
+            this.values[name] = value;
+        }
+
+        public object GetValue(string name)
+        {
+            if (!this.values.ContainsKey(name))
+                if (this.parent != null)
+                    return this.parent.GetValue(name);
+                else
+                    return null;
+            
+            return this.values[name];
+        }
+    }
+}

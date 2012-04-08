f9e81bc3ea30cea54d429931d94f28759931f41b
ajlopez <webmaster@ajlopez.com>
2012-04-08 11:06:32 -0300

    CompositeCommand with test

---
+++ Src/DartSharp.Tests/Commands/CompositeCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DartSharp.Commands;
+using DartSharp.Expressions;
+
+namespace DartSharp.Tests.Commands
+{
+    [TestClass]
+    public class CompositeCommandTests
+    {
+        [TestMethod]
+        public void CreateAndExecuteCompositeCommand()
+        {
+            IEnumerable<ICommand> commands = new ICommand[] {
+                new SetVariableCommand("a", new ConstantExpression(1)),
+                new SetVariableCommand("b", new ConstantExpression(2))
+            };
+
+            CompositeCommand command = new CompositeCommand(commands);
+
+            Context context = new Context();
+
+            Assert.IsNotNull(command.Commands);
+            Assert.AreEqual(2, command.Commands.Count());
+            Assert.AreEqual(2, command.Execute(context));
+            Assert.AreEqual(1, context.GetValue("a"));
+            Assert.AreEqual(2, context.GetValue("b"));
+        }
+    }
+}
---
+++ Src/DartSharp/Commands/CompositeCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace DartSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DartSharp.Methods;
+
+    public class CompositeCommand : ICommand
+    {
+        private IEnumerable<ICommand> commands;
+
+        public CompositeCommand(IEnumerable<ICommand> commands)
+        {
+            this.commands = commands;
+        }
+
+        public IEnumerable<ICommand> Commands { get { return this.commands; } }
+
+        public object Execute(Context context)
+        {
+            object result = null;
+
+            foreach (var command in this.commands)
+                result = command.Execute(context);
+
+            return result;
+        }
+    }
+}

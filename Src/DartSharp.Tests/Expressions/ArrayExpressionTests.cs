a0870467dc5399118c90be860ffd79f56ba42c13
ajlopez <webmaster@ajlopez.com>
2012-04-21 19:20:14 -0300

    ArrayExpression with test

---
+++ Src/DartSharp.Tests/Expressions/ArrayExpressionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DartSharp.Commands;
+using DartSharp.Expressions;
+using DartSharp.Methods;
+using System.IO;
+using System.Collections;
+
+namespace DartSharp.Tests.Expressions
+{
+    [TestClass]
+    public class ArrayExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateArrayExpression()
+        {
+            ArrayExpression expr = new ArrayExpression(new IExpression[] { new ConstantExpression(1), new ConstantExpression(2), new ConstantExpression(3) });
+
+            Assert.IsNotNull(expr.Expressions);
+            Assert.AreEqual(3, expr.Expressions.Count());
+
+            object result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IList));
+
+            IList list = (IList)result;
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+            Assert.AreEqual(3, list[2]);
+        }
+    }
+}
---
+++ Src/DartSharp/Expressions/ArrayExpression.cs
@@ -0,0 +1,32 @@
+﻿namespace DartSharp.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DartSharp.Language;
+
+    public class ArrayExpression : IExpression
+    {
+        private IEnumerable<IExpression> expressions;
+
+        public ArrayExpression(IEnumerable<IExpression> expressions)
+        {
+            this.expressions = expressions;
+        }
+
+        public IEnumerable<IExpression> Expressions { get { return this.expressions; } }
+
+        public object Evaluate(Context context)
+        {
+            IList<object> values = new List<object>();
+
+            if (expressions != null)
+                foreach (var argument in expressions)
+                    values.Add(argument.Evaluate(context));
+
+            return values;
+        }
+    }
+}
+

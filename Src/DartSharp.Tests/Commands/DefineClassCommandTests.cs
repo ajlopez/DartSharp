e5e545abe9943c65071446e228b80689c0467b27
ajlopez <webmaster@ajlopez.com>
2012-04-22 19:21:59 -0300

    DefineClassCommand with tests

---
+++ Src/DartSharp.Tests/Commands/DefineClassCommandTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DartSharp.Commands;
+using DartSharp.Language;
+
+namespace DartSharp.Tests.Commands
+{
+    [TestClass]
+    public class DefineClassCommandTests
+    {
+        [TestMethod]
+        public void CreateDefineClassCommand()
+        {
+            DefineClassCommand command = new DefineClassCommand("MyClass", null);
+            Assert.AreEqual("MyClass", command.Name);
+            Assert.IsNull(command.Command);
+        }
+
+        [TestMethod]
+        public void ExecuteDefineClassCommand()
+        {
+            Context context = new Context();
+            DefineClassCommand command = new DefineClassCommand("MyClass", null);
+            command.Execute(context);
+            var result = context.GetValue("MyClass");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IClass));
+
+            IClass klass = (IClass)result;
+            Assert.AreEqual("MyClass", klass.Name);
+        }
+    }
+}
---
+++ Src/DartSharp/Commands/DefineClassCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace DartSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DartSharp.Methods;
+    using DartSharp.Language;
+
+    public class DefineClassCommand : ICommand
+    {
+        private string name;
+        private ICommand command;
+
+        public DefineClassCommand(string name, ICommand command)
+        {
+            this.name = name;
+            this.command = command;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public ICommand Command { get { return this.command; } }
+
+        public object Execute(Context context)
+        {
+            IType type = new BaseClass(this.name, (IClass)context.GetValue("Object"));
+            context.SetValue(this.name, type);
+            return null;
+        }
+    }
+}
